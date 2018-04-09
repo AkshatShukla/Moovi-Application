828d84fdab99bc14ac2e2ae7981886a88359f7e0
ankitbaheti <ankit.baheti04@gmail.com>
2018-04-09 19:34:48 -0400

    modfied AdRecruiter service and repository classes
    added create, findall and finduserbyusername methods

--- moovi/src/main/java/com/dbms/project/moovi/business/service/AdRecruiterService.java
+++ moovi/src/main/java/com/dbms/project/moovi/business/service/AdRecruiterService.java
@@ -3,16 +3,28 @@ package com.dbms.project.moovi.business.service;
 import com.dbms.project.moovi.data.entity.AdRecruiter;
 import com.dbms.project.moovi.data.repository.AdRecruiterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public class AdRecruiterService extends APICredentials {
 
     @Autowired
     AdRecruiterRepository adRecruiterRepository;
 
-    @PostMapping
+    @PostMapping("/api/adrecruiter")
     public AdRecruiter createAdRecruiter(@RequestBody AdRecruiter adRecruiter){
         return adRecruiterRepository.save(adRecruiter);
     }
+
+    @GetMapping("/api/adrecruiter")
+    public List<AdRecruiter> findAllAdRecruiters(@RequestParam(name="username", required = false) String username){
+        if(username != null)
+            return (List<AdRecruiter>) adRecruiterRepository.findUserByUsername(username);
+        return (List<AdRecruiter>) adRecruiterRepository.findAll();
+    }
+
 }
--- moovi/src/main/java/com/dbms/project/moovi/data/repository/AdRecruiterRepository.java
+++ moovi/src/main/java/com/dbms/project/moovi/data/repository/AdRecruiterRepository.java
@@ -1,7 +1,12 @@
 package com.dbms.project.moovi.data.repository;
 
 import com.dbms.project.moovi.data.entity.AdRecruiter;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface AdRecruiterRepository extends CrudRepository<AdRecruiter, Integer>{
+
+    @Query("SELECT a FROM AdRecruiter a WHERE a.username=:username")
+    Iterable<AdRecruiter> findUserByUsername(@Param("username") String u);
 }
