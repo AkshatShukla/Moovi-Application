252468fc5934d83de981b82ebfc3c3f82215f521
akshatshukla <aks.shukla47@gmail.com>
2018-04-10 17:48:52 -0400

    Added methods to ActorService
    - Added createActor method with post mapping

--- moovi/src/main/java/com/dbms/project/moovi/business/service/ActorService.java
+++ moovi/src/main/java/com/dbms/project/moovi/business/service/ActorService.java
@@ -1,6 +1,9 @@
 package com.dbms.project.moovi.business.service;
 
+import com.dbms.project.moovi.data.entity.Actor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dbms.project.moovi.data.repository.ActorRepository;
@@ -14,5 +17,11 @@ public class ActorService extends Utils{
 	
 	@Autowired
     private FanRepository fanRepository;
+
+	@PostMapping("/api/actor")
+    public Actor createActor(@RequestBody Actor actor) {
+	    return actorRepository.save(actor);
+    }
+
 
 }
