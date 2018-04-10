eedb239d3d2fa8534d765deb9fb024135014532c
ankitbaheti <ankit.baheti04@gmail.com>
2018-04-10 19:42:57 -0400

    creater TheatreManager Repository and TheatreManager Service

--- moovi/src/main/java/com/dbms/project/moovi/business/service/TheatreManagerService.java
+++ moovi/src/main/java/com/dbms/project/moovi/business/service/TheatreManagerService.java
@@ -1,4 +1,20 @@
 package com.dbms.project.moovi.business.service;
 
+import com.dbms.project.moovi.data.entity.TheatreManager;
+import com.dbms.project.moovi.data.repository.TheatreManagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TheatreManagerService extends Utils {
+
+    @Autowired
+    private TheatreManagerRepository theatreManagerRepository;
+
+    @PostMapping("/api/critic")
+    public TheatreManager createTheatreManager(@RequestBody TheatreManager theatreManager) {
+        return theatreManagerRepository.save(theatreManager);
+    }
 }
---
+++ moovi/src/main/java/com/dbms/project/moovi/data/repository/TheatreManagerRepository.java
@@ -0,0 +1,7 @@
+package com.dbms.project.moovi.data.repository;
+
+import com.dbms.project.moovi.data.entity.TheatreManager;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TheatreManagerRepository extends CrudRepository<TheatreManager, Long> {
+}
