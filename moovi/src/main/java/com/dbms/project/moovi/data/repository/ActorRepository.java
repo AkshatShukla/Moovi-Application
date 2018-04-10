0010a8f49cb33a8a519e90d0209f9537b99b91e4
ankitbaheti <ankit.baheti04@gmail.com>
2018-04-09 20:38:53 -0400

    created actor repository

---
+++ moovi/src/main/java/com/dbms/project/moovi/data/repository/ActorRepository.java
@@ -0,0 +1,7 @@
+package com.dbms.project.moovi.data.repository;
+
+import com.dbms.project.moovi.data.entity.Actor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Long> {
+}
