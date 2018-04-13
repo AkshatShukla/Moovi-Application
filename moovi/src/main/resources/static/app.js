74e5b3d46bbac14b70d300f12065c6b345f28248
akshatshukla <aks.shukla47@gmail.com>
2018-04-12 20:14:06 -0400

    Added changes to index.html and app.js
    - onLoading page gives us Now Playing movies

--- moovi/src/main/resources/static/app.js
+++ moovi/src/main/resources/static/app.js
@@ -2,23 +2,41 @@
     angular
         .module("MooviApp", [])
         .controller("MovieSearchController", MovieSearchController);
-    
-    function MovieSearchController($http, $scope) {
+
+    function MovieSearchController($http, $scope, $window) {
         var vm = this;
+        var localpath = "http://localhost:8080/";
+        var url = "/api/search/movie";
         vm.searchMovieByTitle = searchMovieByTitle;
+
+        $window.onload = function () {
+            var nowPlayingUrl = "?nowPlaying=true";
+            $scope.myVal = false;
+            $http
+                .get(url+nowPlayingUrl)
+                .then(function (value) {
+                    $scope.nowplayingheading = "Now Playing Movies";
+                    $scope.movies = value.data;
+                })
+        };
+
+        $scope.myKeyPress = function(keyEvent,title) {
+            if (keyEvent.which === 13)
+                searchMovieByTitle(title);
+        };
         
         function searchMovieByTitle(title) {
             //var url = "https://api.themoviedb.org/3/search/movie?api_key=878a88feb1d8acab0c9883e805657264&query="+title;
-            var localpath = "http://localhost:8080/";
-            var url = "/api/search/movie?movieName="+title;
+            var findByTitle = "?movieName="+title;
+            $scope.myVal = true;
             $http
-                .get(url)
+                .get(url+findByTitle)
                 .then(function (response) {
                     console.log(response);
                     $scope.movies = response.data;
                 });
             console.log(title);
-
         }
+
     }
 })();
