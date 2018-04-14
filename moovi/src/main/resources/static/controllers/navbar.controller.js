f4ba98f1bafd1a45f16c8fb2cdbde4920b20b8ea
akshatshukla <aks.shukla47@gmail.com>
2018-04-14 17:20:18 -0400

    Implemented logout for logged in user
    - Navebar now changes view based on user logged in or logged out

--- moovi/src/main/resources/static/controllers/navbar.controller.js
+++ moovi/src/main/resources/static/controllers/navbar.controller.js
@@ -4,8 +4,22 @@
         .controller("NavbarController", NavbarController);
     
     function NavbarController($scope) {
-
+        var vm = this;
         $scope.typeRole = localStorage.getItem("userType");
+        alert($scope.typeRole);
+        console.log($scope.typeRole);
+        $scope.foo = null;
+
+        $scope.getFooUndef = function(foo){
+            return ( typeof foo == null );
+        };
+
+        vm.resetUserType = resetUserType;
+        function resetUserType() {
+            localStorage.removeItem("userType");
+            window.location.href = "#";
+            location.reload(true);
+        }
     }
 
 })();
