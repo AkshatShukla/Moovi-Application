77985edae824f494dbde55343d50944b624803ea
ankitbaheti <ankit.baheti04@gmail.com>
2018-04-14 19:17:07 -0400

    controller for admin to control movie was created along with html file

--- moovi/src/main/resources/static/controllers/editmovie.controller.js
+++ moovi/src/main/resources/static/controllers/editmovie.controller.js
@@ -7,5 +7,22 @@
         var vm = this;
         var localpath = "http://localhost:8080/";
         alert("hi from EditMovieController");
+
+        vm.addMovie = addMovie;
+
+        $scope.$on('$viewContentLoaded', function()
+        {
+            var allMovieUrl = "api/movie";
+            $http
+                .get(localpath+allMovieUrl)
+                .then(function (value) {
+                    $scope.movies = value.data;
+                })
+
+        });
+
+        function addMovie(movieName, overview, posterSrc, releaseDate, imdbRating, revenue, runtime, releaseStatus, imdbId) {
+
+        }
     }
 })();
