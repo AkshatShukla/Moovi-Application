2988d2b30660ec7c645c9d2522d206e93eb16511
virat02 <goradia.v@husky.neu.edu>
2018-04-14 19:47:50 -0400

    list of critics followed implemented succesfully
    spelling error created error. All critics followed by the fan are retrieved successfully

--- moovi/src/main/resources/static/controllers/listofcriticsfollowing.controller.js
+++ moovi/src/main/resources/static/controllers/listofcriticsfollowing.controller.js
@@ -6,7 +6,6 @@
     function CriticFollowingController($http, $scope) {
 
         var vm = this;
-        alert("Hello Critic Following!");
         var localpath = "http://localhost:8080/";
         var url1 = "api/follow/fan/";
         var url2 = "/criticfollowed";
@@ -14,7 +13,7 @@
         $scope.$on('$viewContentLoaded', function()
         {
             $http
-                .get(localpath+url1+localStorage.getItem('username')+url2)
+                .get(localpath+url1+localStorage.getItem("username")+url2)
                 .then(function (response) {
                     $scope.allCriticsHeading = "All Critics You Follow";
                     $scope.critics = response.data;
