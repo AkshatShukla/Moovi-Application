0e3ba816913cff85f5264f2bda8c90132b339f1a
virat02 <goradia.v@husky.neu.edu>
2018-04-15 08:26:42 -0400

    implemented CRUD operation of fan's following list for admin
    admin can now access a fan's following list, and can make them unfollow their followed fans

--- moovi/src/main/resources/static/controllers/listoffansfollowing.controller.js
+++ moovi/src/main/resources/static/controllers/listoffansfollowing.controller.js
@@ -10,17 +10,55 @@
         var url1 = "api/follow/fan/";
         var url2 = "/fansfollowing";
 
+        $scope.usT = localStorage.getItem("userType");
+        $scope.fanname = localStorage.getItem("username");
+        $scope.fanname1 = localStorage.getItem("DirectedUserName");
+
         $scope.$on('$viewContentLoaded', function()
         {
-            $http
-                .get(localpath+url1+localStorage.getItem('username')+url2)
-                .then(function (response) {
-                    $scope.allFansHeading = "All Fans You Follow";
-                    $scope.fans = response.data;
-                    console.log(response);
-                })
+            if ($scope.usT === 'admin') {
+                $http
+                    .get(localpath + url1 + localStorage.getItem("DirectedUserName") + url2)
+                    .then(function (response) {
+                        $scope.allFansHeading = "All Fans " + localStorage.getItem("DirectedUserName") + " Follows";
+                        $scope.fans = response.data;
+                        console.log(response);
+                    })
+            }
+            else {
 
+                $http
+                    .get(localpath + url1 + localStorage.getItem('username') + url2)
+                    .then(function (response) {
+                        $scope.allFansHeading = "All Fans You Follow";
+                        $scope.fans = response.data;
+                        console.log(response);
+                    })
+            }
         });
 
+        var unfollowURL;
+        if ($scope.usT === 'admin') {
+            unfollowURL = "api/delete/following/fanfollowing/"+$scope.fanname1+"/fanfollowed/";
+        }
+        else {
+            unfollowURL = "api/delete/following/fanfollowing/"+$scope.fanname+"/fanfollowed/";
+        }
+
+        vm.unfollowFan = unfollowFan;
+
+        function unfollowFan(fanUsername) {
+            $http
+                .post(localpath + unfollowURL + fanUsername)
+                .then(function (response) {
+                    $scope.allFansHeading = "All Critics You Follow";
+                    $scope.fans = response.data;
+                    alert("Fan unfollowed");
+                    location.reload(true);
+                })
+        }
+
     }
+
+
 })();
