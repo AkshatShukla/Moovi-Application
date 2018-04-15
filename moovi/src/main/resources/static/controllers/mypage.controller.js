302abdd68229e59a17dc4b61757fbf7c5512907b
virat02 <goradia.v@husky.neu.edu>
2018-04-15 07:53:33 -0400

    implemented edit list of critics followed by fan for admin
    Now an admin can access a fan's list of critics they follow and make them unfollow their followed critics

--- moovi/src/main/resources/static/controllers/mypage.controller.js
+++ moovi/src/main/resources/static/controllers/mypage.controller.js
@@ -7,6 +7,11 @@
         var vm = this;
 
         $scope.uT = localStorage.getItem("userType");
-        $scope.f = localStorage.getItem("fanUsername");
+        $scope.f = localStorage.getItem("DirectedUserName");
+        $scope.d = localStorage.getItem("DirectedUserType");
+
+        console.log("d:"+$scope.d);
+        console.log("f:"+$scope.f);
+        console.log("uT:"+$scope.uT);
     }
 })();
